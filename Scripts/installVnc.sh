ae419d82af564381015323d018adc875b834948e
Isaac Elmore <isaac.elmore@gmail.com>
2019-07-10 18:46:44 -0400

    Updated installVnc.sh (other version didn't work).

--- Scripts/installVnc.sh
+++ Scripts/installVnc.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
-sudo apt-get install -y realvnc-vnc-server # Install VNC
+wget https://www.realvnc.com/download/file/vnc.files/VNC-Server-6.4.1-Linux-ARM.deb
+dpkg -i VNC-Server-6.4.1-Linux-ARM.deb
+sudo apt-get install -f
 sudo systemctl start vncserver-x11-serviced.service # Start VNC now
 sudo systemctl enable vncserver-x11-serviced.service # Start VNC on every bootup
