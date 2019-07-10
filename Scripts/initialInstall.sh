5da6c42d4960b10687d73d4fb5f8cc95d3fd5c57
Isaac Elmore <isaac.elmore@gmail.com>
2019-07-10 18:08:20 -0400

    Added initialInstall.sh file (run this to install everything), and removed 'update' line from installVnc.sh.

---
+++ Scripts/initialInstall.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+sudo apt-get update -y
+sudo apt-get upgrade -y
+sudo apt install -y git
+git config --global user.email "isaac.elmore@gmail.com"
+git config --global user.name "Isaac Elmore"
+sudo apt autoremove -y
+
+# Now perform the device specific installs, while outputting 
+# their data to a file and the terminal.
+sudo ./installVnc.sh 2>&1 | tee vncInstallResults.txt
+sudo ./installOpenCv.sh 2>&1 | tee opencvInstallResults.txt
--- Scripts/installVnc.sh
+++ Scripts/installVnc.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
-sudo apt-get update # Update the device
 sudo apt-get install -y realvnc-vnc-server # Install VNC
 sudo systemctl start vncserver-x11-serviced.service # Start VNC now
 sudo systemctl enable vncserver-x11-serviced.service # Start VNC on every bootup
